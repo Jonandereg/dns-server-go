299e9a47e08f58a8f7a2534384e519f09c8f81f0
jon-elosua_qonto <jon.elosua@qonto.com>
2026-03-16 13:21:59 +0100

    first implementation of writing header

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -32,11 +33,63 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := []byte{}
+		dnsMessage := DNSMessage{}
+		response := dnsMessage.writeResponse()
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 	}
+}
+
+type DNSMessage struct {
+	header   []byte
+	question []byte
+	answer   []byte
+}
+
+func (dm *DNSMessage) writeResponse() []byte {
+	response := make([]byte, 512)
+	dm.writeHeader()
+	response = append(response, dm.header...)
+
+	return response
+}
+func (dm *DNSMessage) writeHeader() {
+	header := make([]byte, 12)
+	id := uint16(1234)
+	binary.BigEndian.PutUint16(header[0:2], id)
+	flags := buildFlags()
+	binary.BigEndian.PutUint16(header[2:4], flags)
+	qCount := uint16(0)
+	binary.BigEndian.PutUint16(header[4:6], qCount)
+	anCount := uint16(0)
+	binary.BigEndian.PutUint16(header[6:8], anCount)
+	nsCount := uint16(0)
+	binary.BigEndian.PutUint16(header[8:10], nsCount)
+	arCount := uint16(0)
+	binary.BigEndian.PutUint16(header[10:12], arCount)
+	dm.header = append(dm.header, header...)
+}
+
+func buildFlags() uint16 {
+	flags := uint16(0)
+	qr := uint16(1)
+	flags |= qr << 15
+	opcode := uint16(0)
+	flags |= opcode << 11
+	aa := uint16(0)
+	flags |= aa << 10
+	truncation := uint16(0)
+	flags |= truncation << 9
+	recursionDesired := uint16(0)
+	flags |= recursionDesired << 8
+	recursionAvailable := uint16(0)
+	flags |= recursionAvailable << 7
+	// 3 bits reserved
+	rCode := uint16(0)
+	flags |= rCode
+
+	return flags
 }
